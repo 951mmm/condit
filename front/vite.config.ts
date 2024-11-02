77805866a32c843b00ce2a114d0eb19fdc0e6c49
www159 <2107795244@qq.com>
2024-11-02 20:02:58 +0800

    chore: packed

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:2333', // 后端服务器地址
-        changeOrigin: true,              // 支持跨域
-      },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: {
+      proxy:
+        mode === "development"
+          ? {
+              "/api": {
+                target: "http://localhost:2333", // 后端服务器地址
+                changeOrigin: true, // 支持跨域
+              },
+            }
+          : {},
     },
-  },
-})
+    build: {
+      minify: mode === "development" ? false: true
+    }
+  };
+});
